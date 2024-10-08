64490ec27abbf3351001add3a6f72da0e06626f3
GubkinaKris13 <christinagubkina@yandex.ru>
2024-10-08 20:10:52 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+from os import replace
+
+my_string = input('Какой сегодня день? ')
+print(len(my_string))
+print(my_string .upper())
+print(my_string .lower())
+print(my_string .replace(' ' , ''))
+print(my_string[0])
+print(my_string[-1])
